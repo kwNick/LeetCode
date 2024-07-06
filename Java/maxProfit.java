d7b4dee16b2e8e66e15b808f9f02939c49110e63
Nickolas Piraino <kwnp17c@gmail.com>
2024-07-06 14:16:04 -0400

    linear difference check between array elements

---
+++ Java/maxProfit.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int prof = 0;
+        for (int i = 0; i < prices.length; i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }else if(prices[i] - min > prof){
+                prof = prices[i] -min;
+            }
+        }
+        return prof;
+    }
+}
