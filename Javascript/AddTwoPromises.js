24e8466be60744c92558be0e19b181f88bee60ab
Nickolas Piraino <kwnp17c@gmail.com>
2024-08-19 16:21:53 -0400

    awaiting each individual promise one after the other; should be slower than promise.all

--- Javascript/AddTwoPromises.js
+++ Javascript/AddTwoPromises.js
@@ -4,9 +4,18 @@
  * @return {Promise}
  */
 var addTwoPromises = async function (promise1, promise2) {      //O(max(promise1,promise2]))
-    try {
+    try {   //Promise.all() concurrently running all promises 
         const [res1, res2] = await Promise.all([promise1, promise2]);
         return res1 + res2;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+var addTwoPromises_2 = async function (promise1, promise2) {      //leet code says: O(max(promise1,promise2])) but i think its O(promise1+promise2)
+    try {      //awaiting the promises one after the other
+        return await promise1 + await promise2;
     } catch (error) {
         console.error(error);
         throw error;
