9fe28f73d9560ba39e60600252c9e64dabea9c90
Nickolas Piraino <kwnp17c@gmail.com>
2024-08-27 14:40:56 -0400

    traverse and array for operations javascript

--- Javascript/MinimumOperations.js
+++ Javascript/MinimumOperations.js
@@ -5,3 +5,17 @@
 var minimumOperations = function (nums) {
     return nums.reduce((acc, cur) => cur % 3 != 0 ? acc += 1 : acc += 0, 0,);
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations_2 = function (nums) {
+    let count = 0;
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] % 3 != 0)
+            count++;
+    }
+    return count;
+};
