c58b59a02b1e63cf1f8e64c956ab06112b063e2a
Nickolas Piraino <kwnp17c@gmail.com>
2024-08-31 23:30:37 -0400

    iterating a doubly linked list and pushing the values to an array

---
+++ Javascript/DListToArray.js
@@ -0,0 +1,22 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val,prev,next) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ * };
+ */
+
+/**
+ * @param {_Node} head
+ * @return {number[]}
+ */
+var toArray = function (head) {
+    let arr = [];
+    let tmp = head;
+    while (tmp != null) {
+        arr.push(tmp.val);
+        tmp = tmp.next;
+    }
+    return arr;
+};
