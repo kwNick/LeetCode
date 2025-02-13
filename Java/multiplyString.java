ea912ab928cdaa0ed71c8a3a48ffbc71e53367d5
Nickolas Piraino <kwnp17c@gmail.com>
2025-02-12 19:28:59 -0500

    multiply strings in leet code java

---
+++ Java/multiplyString.java
@@ -0,0 +1,7 @@
+class multiplyString {
+    public String multiply(String num1, String num2) {
+        return Integer.toString(Integer.parseInt(num1) * Integer.parseInt(num2));
+    }
+}
+
+//parses to number multiplies them and parses back to string
